cbd050f0eea44420e0ad0f43f0817b94716659d4
kkterai <kenlyn.terai@gmail.com>
2020-01-12 16:17:48 -0800

    Connect MongoDB

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 //  Import the Express library
 const express = require('express');
+// Import database
+const mongoose = require('mongoose');
 // Next, create an instance of express, or "Instantiate" a router
 const app = express();
 
+// Db Config
+const db = require('./config/keys').mongoURI;
+// Connect to mongodb
+mongoose
+    .connect(db)
+    .then(() => console.log("MongoDb connected!"))
+    .catch(err => console.log(err));
+
 // First route
-app.get('/', (req, res) => res.send('Hello'));
+app.get('/', (req, res) => res.send('Hello World'));
 
 // Open a port - Pick one greater than 5000
 const port = 5555;
