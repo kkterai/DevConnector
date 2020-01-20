5e256bb6a8af3e18c367d40d76732ddbc6811c98
kkterai <kenlyn.terai@gmail.com>
2020-01-19 16:28:20 -0800

    Define POST route for user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const User = require('../../models/User');
 const router = express.Router();
+
 
 // Named functions will become the route names
 router.get('/test', (req,res) => res.json({
-    msg: 'Users api works!'
+    msg: "Users test works!"
 }))
 
+// @route   POST api/users/register
+// @desc    Register user
+// @access  public
+router.post('/register', (req,res) => {
+    User.findOne({email: req.body.email})
+    .then(user => {
+        if (user){
+            return res.status(400).json({
+                email: 'Email already  exists'
+            })
+        } else {
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password // TODO: Encrypt password
+            });
+            newUser.save()
+                .then(user => res.json(user))
+                .catch(err => console.log(err));
+        }
+    }) // success
+    .catch(err => console.log(err)); // failure
+});
 module.exports = router;
