015c5a59da899a26225f2ba5624adc755ef1959e
kkterai <kenlyn.terai@gmail.com>
2020-02-23 01:07:28 -0800

    Remove extra comments

--- server.js
+++ server.js
@@ -1,41 +1,36 @@
-//  Import the Express library
-const express = require('express');
-// Import database
-const mongoose = require('mongoose');
-// Import route JS files
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
+const express = require("express");
+const mongoose = require("mongoose");
+const users = require('./routes/api/users')
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 const bodyparser = require('body-parser');
 const passport = require('passport');
-// Next, create an instance of express, or "Instantiate" a router
 const app = express();
 
-// Body parser middleware - Defined at the app top-level to be used throughout
-app.use(bodyparser.urlencoded({extended: false}
-));
+//Body parser middleware
+app.use(bodyparser.urlencoded({extended: false}));
 app.use(bodyparser.json());
 
-// Db Config
-const db = require('./config/keys').mongoURI;
-// Connect to mongodb
+//Db Config
+const db = require("./config/keys.js").mongoURI;
+
+//Connect to mongodb
 mongoose
-    .connect(db)
-    .then(() => console.log("MongoDb connected!"))
-    .catch(err => console.log(err));
+  .connect(db)
+  .then(() => console.log("MongoDb connected!"))
+  .catch(err => console.log(err));
 
 //Passport middleware
 app.use(passport.initialize());
+require('./config/passport')(passport);
 
-// First route
-app.get('/', (req, res) => res.send('Hello World'));
-// Use routes
-app.use('/api/users', users);
+//First route
+app.get("/", (req, res) => res.send("Hello"));
+
+//Use routes
+app.use('/api/users', users)
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-// Open a port - Pick one greater than 5000
 const port = 5555;
-
-// Tell the router to listen on 5555
 app.listen(port, () => console.log(`Server running on port ${port}`));
