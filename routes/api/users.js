b7db8084e81723351f944309d1f6fcc2123da0b4
kkterai <kenlyn.terai@gmail.com>
2020-03-01 07:05:37 -0800

    Add client code per class

--- routes/api/users.js
+++ routes/api/users.js
@@ -19,7 +19,7 @@ router.post("/register", (req, res) => {
   if (!isValid){
     return res.status(400).json(errors);
   }
-  User.findOne({ email: req.body.email })
+  User.findOne({ email: req.body.email }) // Equivalent to 'where' statement in sql
     .then(user => {
       if (user) {
         errors.email = "Email already exists";
