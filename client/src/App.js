62632651ebb593fe52d7a0b0185bc47b6ebc2669
kkterai <kenlyn.terai@gmail.com>
2020-03-02 22:26:52 -0800

    Add Redux Provider and store

--- client/src/App.js
+++ client/src/App.js
@@ -6,21 +6,25 @@ import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Provider from 'react-redux';
+import store from './store';
 
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-          <Footer />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
