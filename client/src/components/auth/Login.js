0c3a680c5caaeff605fcd079a380aca0d5956363
kkterai <kenlyn.terai@gmail.com>
2020-03-09 21:50:09 -0700

    Finish implementing form UI for login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -30,7 +30,7 @@ class Login extends Component {
 
         axios
             .post('/api/users/login', user)
-            .then(res => console.log("token"))
+            .then(res => console.log(res.data.token))
             .catch(err => this.setState({errors: err.response.data}));
     }
 
@@ -48,22 +48,28 @@ class Login extends Component {
                         <div className="form-group">
                         <input type="email"   
                         className={classnames("form-control form-control-lg", {
-                            "is-invalid": errors.name
+                            "is-invalid": errors.email
                             })}  
-                        placeholder="Email Address" name="email" 
+                        placeholder="Email Address" 
+                        name="email" required
                         value={this.state.email}
-                        onChange={this.onChange}
-                        />
+                        onChange={this.onChange} />
+                        {errors.email && (
+                            <div className="invalid-feedback">{errors.email}</div>
+                        )}
                         </div>
                         <div className="form-group">
                         <input type="password" 
                           className={classnames("form-control form-control-lg", {
-                            "is-invalid": errors.name
+                            "is-invalid": errors.password
                             })} 
-                        placeholder="Password" name="password" 
+                        placeholder="Password" 
+                        name="password" 
                         value={this.state.password}
-                        onChange={this.onChange}
-                        />
+                        onChange={this.onChange} />
+                        {errors.password && (
+                            <div className="invalid-feedback">{errors.password}</div>
+                        )}
                         </div>
                         <input type="submit" className="btn btn-info btn-block mt-4" />
                     </form>
