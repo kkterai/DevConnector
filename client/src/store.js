7ad43071cbb7ef63b0c7630a24870d92a4905e29
kkterai <kenlyn.terai@gmail.com>
2020-03-09 21:54:14 -0700

    Connect app to Redux dev tools

--- client/src/store.js
+++ client/src/store.js
@@ -1,11 +1,13 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
+import rootReducer from './reducers/index';
 
 const middleware = [thunk];
 
 const store = createStore(
-    () => [], 
+    rootReducer, 
     {}, 
-    applyMiddleware(...middleware));
-
+    compose(applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    );
 export default store;
