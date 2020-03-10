4dc36b893fe9e61fd2b57ce5f6860b17d0039464
kkterai <kenlyn.terai@gmail.com>
2020-03-09 21:52:08 -0700

    Finish implementing authReducer

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,5 +1,18 @@
-const initialState = {
-    isAuthenticated: false, 
-    user: {}
-}
+import {SET_CURRENT_USER} from '../actions/types';
 
+const initialState = {
+    isAuthenticated: false,
+    user: {}
+  };
+  
+export default function(state = initialState, action){
+    switch(action.type) {
+        case SET_CURRENT_USER:
+            return {
+                ...state,
+                user: action.payload
+            }
+        default: 
+        return state;
+    }
+}
